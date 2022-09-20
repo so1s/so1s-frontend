343690e00d08f1ae6b90b1ccf3cbb6eb016936c8
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-20 14:24:37 +0900

    feat: set host, port
    Co-authored-by: Jiho Lee <optional.int@kakao.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0"
+  }
 })
