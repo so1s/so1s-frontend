99ff8a340178a7998518401990a98146e8378eb8
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-21 18:49:22 +0900

    chore: set color config
    Co-authored-by: Jiho Lee <optional.int@kakao.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: "#0A3B71",
+      background: "#F5F5F5",
+      error: "#F45452",
+      white: "#FCFCFC",
+      body: "#5C5C5C",
+      title: "#1E2222",
+      label: "#888888",
+      placeholder: "#BBBBBB",
+      line: "#D7D7D7"
+    }
   },
   plugins: [],
 }
