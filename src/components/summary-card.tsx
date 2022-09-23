19de2f7571507a4afb40a27547507cbafc8ac6d1
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-23 22:19:47 +0900

    fix: summary card style

--- src/components/summary-card.tsx
+++ src/components/summary-card.tsx
@@ -2,26 +2,29 @@ import { Paper } from '@mui/material';
 import { ISummaryCardProps } from '../interfaces/components/summary-card';
 
 const SummaryCard: React.FC<ISummaryCardProps> = ({
-    title,
+    title = 'nsns',
     summaryList,
     icon,
 }) => {
     return (
-        <Paper className="w-52">
+        <Paper className="w-64">
             <div className="text-1xl p-3 border-b-2 border-line border-1">
                 {title}
             </div>
-            <div className="flex my-3">
+            <div className="flex flex-col my-3">
                 {summaryList.map((item, idx) => {
                     return (
                         <div
-                            className="flex space-x-5 mx-3 justify-center"
+                            className="flex space-x-5 mx-3 my-3 justify-center"
                             key={idx}
                         >
                             {icon}
-                            <div className="text-xs">
+                            <div className="text-xs flex-1">
                                 <div> {item.title} </div>
-                                <div> {item.content} </div>
+                                <div className="text-label">
+                                    {' '}
+                                    {item.content}{' '}
+                                </div>
                             </div>
                         </div>
                     );
