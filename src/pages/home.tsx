a8304c97e61706eb503482195406eb4044051409
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-23 22:20:08 +0900

    feat: fill model list

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,53 @@
-import { Paper } from '@mui/material';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import { useEffect, useState } from 'react';
+import getModels from '../api/models';
+import SummaryCard from '../components/summary-card';
+import { IModelFindResponse } from '../interfaces/models';
+import { Summary } from '../types/components/summary-card';
 
 const Home: React.FC = () => {
+    const [modelSummary, setModelSummary] = useState<Summary[]>([]);
+
+    useEffect(() => {
+        const getModelData = async () => {
+            const response: IModelFindResponse[] = await getModels();
+
+            const result = response.map((item) => {
+                return {
+                    title: item.name,
+                    content: item.status,
+                };
+            });
+
+            setModelSummary(result);
+        };
+
+        getModelData();
+    }, []);
+
     return (
         <div className="flex flex-row justify-around">
-            <Paper>Hello World!</Paper>
-            <Paper>Hello World!</Paper>
-            <Paper>Hello World!</Paper>
+            <SummaryCard
+                title="Model Summary"
+                summaryList={modelSummary}
+                icon={
+                    <RocketLaunchIcon fontSize="medium" className="my-auto" />
+                }
+            />
+            <SummaryCard
+                title="Deployment Summary"
+                summaryList={[{ title: 'test', content: 'SUCCEEDED' }]}
+                icon={
+                    <RocketLaunchIcon fontSize="medium" className="my-auto" />
+                }
+            />
+            <SummaryCard
+                title="A/B Test Summary"
+                summaryList={[{ title: 'test', content: 'SUCCEEDED' }]}
+                icon={
+                    <RocketLaunchIcon fontSize="medium" className="my-auto" />
+                }
+            />
         </div>
     );
 };
