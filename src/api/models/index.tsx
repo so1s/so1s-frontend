c163364dd11955eadc6b9f14e690b3bbb0985d25
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-09-27 19:20:06 +0900

    feat: add model yaml request api

--- src/api/models/index.tsx
+++ src/api/models/index.tsx
@@ -8,4 +8,24 @@ export const getModels = async () => {
     return response.data as IModelFindResponse[];
 };
 
+interface IModelYAMLFindRequest {
+    modelId: string | number;
+    version: string | number;
+}
+
+interface IModelYAMLFindResponse {
+    yaml: string;
+}
+
+export const getModelYaml = async ({
+    modelId,
+    version,
+}: IModelYAMLFindRequest) => {
+    const response = await axios.get(
+        `${baseURL}/api/v1/models/${modelId}/versions/${version}/yaml`
+    );
+
+    return response.data as IModelYAMLFindResponse;
+};
+
 export default getModels;
