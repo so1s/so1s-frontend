37fb1a68024fdf0f2aa0ce8941cdb306219aa141
Jiho Lee <optional.int@kakao.com>
2022-09-29 17:50:17 +0900

    feat: add link on logo, login icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,11 +9,11 @@ const Header: React.FC = () => {
     return (
         <header className="sticky top-0 w-full flex justify-between z-30">
             <div className="flex-grow h-18 w-full flex justify-between px-5 py-2 bg-primary text-white">
-                <img src={Logo} className="my-auto h-8" alt="So1s Logo" />
+                <a href="/"> <img src={Logo} className="my-auto h-8" alt="So1s Logo" /></a>
                 <div className="font-serif text-2xl">
                     {page?.name ?? 'Home'}
                 </div>
-                <LogoutIcon className="my-auto" fontSize="large" />
+                <a href="/login"> <LogoutIcon className="my-auto" fontSize="large" /></a>
             </div>
         </header>
     );
