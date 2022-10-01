23ac6405f109a2a5ef4c7e0aef6a12fc4392e571
Jiho Lee <optional.int@kakao.com>
2022-10-02 02:00:56 +0900

    feat: use library provided props

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,32 +1,14 @@
 import { TextField } from '@mui/material';
 import IInputProps from '../interfaces/components/input';
 
-const Input: React.FC<IInputProps> = ({
-    title,
-    type,
-    placholder,
-    helperText,
-    required,
-    defaultValue,
-    disabled,
-    fullWidth,
-    inputRef,
-}) => {
+const Input: React.FC<IInputProps> = ({ title, ...textFieldProps }) => {
     return (
         <div className="my-2">
             {' '}
-            <div className="text-large text-label py-1">{title}</div>
-            <TextField
-                variant="outlined"
-                type={type ?? ''}
-                fullWidth={!fullWidth}
-                placeholder={placholder}
-                helperText={helperText}
-                required={required}
-                defaultValue={defaultValue}
-                disabled={disabled}
-                inputRef={inputRef}
-            />{' '}
+            {title ? (
+                <div className="text-large text-label py-1">{title}</div>
+            ) : null}
+            <TextField variant="outlined" {...textFieldProps} />{' '}
         </div>
     );
 };
