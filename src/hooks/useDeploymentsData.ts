6828e7310556a440a6b08e0ba85305b14d4cf7f0
Jiho Lee <optional.int@kakao.com>
2022-10-09 20:29:55 +0900

    feat: use useData on other hooks

--- src/hooks/useDeploymentsData.ts
+++ src/hooks/useDeploymentsData.ts
@@ -1,32 +1,6 @@
-import { useAtom } from 'jotai';
-import { useEffect } from 'react';
 import getDeployments from '../api/deployments';
 import { deploymentsAtom } from '../atoms/deployments';
-import { IDeploymentDatum } from '../interfaces/pages/deployments';
-import { convertStatusToIcon } from '../utils/pages/models';
+import { useData } from './useData';
 
-export const useDeploymentsData = () => {
-    const [, setDeployments] = useAtom(deploymentsAtom);
-
-    const getData = async () => {
-        try {
-            const data = await getDeployments();
-            setDeployments(
-                data.map(
-                    (datum) =>
-                        ({
-                            ...datum,
-                            age: new Date(datum.age).toLocaleString(),
-                            status: convertStatusToIcon(datum.status),
-                        } as IDeploymentDatum)
-                )
-            );
-        } catch (err) {
-            setTimeout(getData, 200);
-        }
-    };
-
-    useEffect(() => {
-        getData();
-    }, []);
-};
+export const useDeploymentsData = () =>
+    useData(deploymentsAtom, getDeployments);
