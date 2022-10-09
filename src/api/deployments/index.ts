c20e42a98e0cc24c268f58efef6f61eb7d0980c6
Jiho Lee <optional.int@kakao.com>
2022-10-10 00:38:08 +0900

    feat: add createDeploymenOrPut function

--- src/api/deployments/index.ts
+++ src/api/deployments/index.ts
@@ -1,6 +1,8 @@
 import { baseURL } from '../../constants';
 import { axiosInstance } from '../../hooks/useAuth';
 import {
+    IDeploymentCreateRequest,
+    IDeploymentCreateResponse,
     IDeploymentDeleteResponse,
     IDeploymentFindResponse,
 } from '../../interfaces/pages/deployments';
@@ -9,6 +11,18 @@ export const getDeployments = async () => {
     const response = await axiosInstance.get(`${baseURL}/api/v1/deployments`);
 
     return response.data as IDeploymentFindResponse[];
+};
+
+export const createDeploymentOrPut = async (
+    payload: IDeploymentCreateRequest,
+    mode: 'post' | 'put'
+) => {
+    const response = await axiosInstance[mode](
+        `${baseURL}/api/v1/models`,
+        payload
+    );
+
+    return response.data as IDeploymentCreateResponse;
 };
 
 export const deleteDeployment = async (id: number) => {
