673a61400a46a343e866b7ea525062e574f30a9c
Jiho Lee <optional.int@kakao.com>
2022-10-09 20:28:08 +0900

    feat: add deployment delete api

--- src/api/deployments/index.ts
+++ src/api/deployments/index.ts
@@ -1,11 +1,22 @@
 import { baseURL } from '../../constants';
 import { axiosInstance } from '../../hooks/useAuth';
-import { IDeploymentFindResponse } from '../../interfaces/pages/deployments';
+import {
+    IDeploymentDeleteResponse,
+    IDeploymentFindResponse,
+} from '../../interfaces/pages/deployments';
 
 export const getDeployments = async () => {
     const response = await axiosInstance.get(`${baseURL}/api/v1/deployments`);
 
     return response.data as IDeploymentFindResponse[];
+};
+
+export const deleteDeployment = async (id: number) => {
+    const response = await axiosInstance.delete(
+        `${baseURL}/api/v1/deployments/${id}`
+    );
+
+    return response.data as IDeploymentDeleteResponse;
 };
 
 export default getDeployments;
