8725c8f6fde79e5426c28765c635e59d596e066d
Jiho Lee <optional.int@kakao.com>
2022-10-10 00:37:15 +0900

    feat: add deployment create request / response interface

--- src/interfaces/pages/deployments/index.ts
+++ src/interfaces/pages/deployments/index.ts
@@ -27,4 +27,24 @@ export type IDeploymentDatum = IDeploymentBase & {
     status: JSX.Element;
 };
 
+export interface IDeploymentCreateRequest {
+    name: string;
+    modelMetadataId: number;
+    strategy: string;
+    resources: {
+        cpu: string;
+        memory: string;
+        gpu: string;
+        cpuLimit: string;
+        memoryLimit: string;
+        gpuLimit: string;
+    };
+}
+
+export interface IDeploymentCreateResponse {
+    success: boolean;
+    id: number;
+    name: string;
+}
+
 export interface IDeploymentDeleteResponse extends IBaseResponse {}
