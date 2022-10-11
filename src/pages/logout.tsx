facbb297a1bcdd93710862a4ab61d94d1e4274da
Jiho Lee <optional.int@kakao.com>
2022-10-11 16:11:12 +0900

    chore: delete unused variable

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { accessTokenWithPersistence } from '../atoms/token';
 
 const Logout: React.FC = () => {
-    const [accessToken, setAccessToken] = useAtom(accessTokenWithPersistence);
+    const [, setAccessToken] = useAtom(accessTokenWithPersistence);
     const navigate = useNavigate();
 
     useEffect(() => {
