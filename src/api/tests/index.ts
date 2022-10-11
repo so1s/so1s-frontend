f9df21fb2ae393dcf9c6c806c0539acb2b3b1105
Jiho Lee <optional.int@kakao.com>
2022-10-12 05:04:01 +0900

    feat: add ab tests create, update, delete api

--- src/api/tests/index.ts
+++ src/api/tests/index.ts
@@ -1,12 +1,36 @@
-import axios from 'axios';
 import { baseURL } from '../../constants';
 import { axiosInstance } from '../../hooks/useAuth';
-import { IABTestReadResponse } from '../../interfaces/pages/tests';
+import {
+    IABTestCreateRequest,
+    IABTestCreateResponse,
+    IABTestDeleteResponse,
+    IABTestReadResponse,
+} from '../../interfaces/pages/tests';
 
 export const getABTests = async () => {
     const response = await axiosInstance.get(`${baseURL}/api/v1/tests/ab`);
 
     return response.data as IABTestReadResponse[];
+};
+
+export const createOrUpdateABTest = async (
+    payload: IABTestCreateRequest,
+    mode: 'post' | 'put' = 'post'
+) => {
+    const response = await axiosInstance[mode](
+        `${baseURL}/api/v1/tests/ab`,
+        payload
+    );
+
+    return response.data as IABTestCreateResponse;
+};
+
+export const deleteABTest = async (id: number) => {
+    const response = await axiosInstance.delete(
+        `${baseURL}/api/v1/tests/ab/${id}`
+    );
+
+    return response.data as IABTestDeleteResponse;
 };
 
 export default getABTests;
