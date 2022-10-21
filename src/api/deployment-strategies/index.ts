a51678bd59f9eb1387ba777e68aac84ba7fb2b12
Jiho Lee <optional.int@kakao.com>
2022-10-21 20:14:15 +0900

    feat: use proxy for mutable reference

--- src/api/deployment-strategies/index.ts
+++ src/api/deployment-strategies/index.ts
@@ -1,8 +1,9 @@
 import { baseURL } from '../../constants';
-import { axiosInstance } from '../../hooks/useAuth';
+import { axiosInstanceRef } from '../../hooks/useAuth';
 import { IDeploymentStrategyFindResponse } from '../../interfaces/pages/deployment-strategies';
 
 export const getDeploymentStrategies = async () => {
+    const { current: axiosInstance } = axiosInstanceRef;
     const response = await axiosInstance.get(
         `${baseURL}/api/v1/deployment-strategies`
     );
