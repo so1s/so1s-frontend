0600628674b985f99e7fd15d411bc24ec42d5d03
Jiho Lee <optional.int@kakao.com>
2022-11-17 02:44:56 +0900

    feat: add logging navigation page

---
+++ src/pages/logging/index.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+export const Logging = () => {
+    useEffect(() => {
+        window.location.replace('https://kibana.so1s.io');
+    }, []);
+
+    return <></>;
+};
